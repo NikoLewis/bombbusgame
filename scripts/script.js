4ec673ee4589e77a482202990b70bd297fd385b5
Niko Lewis <nikolewis01@gmail.com>
2018-01-05 10:39:08 -0500

    Connecting views.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,6 @@
 console.log('intro.js is connected');
+
+//had trouble trying to attach an inline 'onclick' to the my image input. Learned how to use the location object instead from w3c school https://www.w3schools.com/jsref/obj_location.asp
+$('.button').click(function(argument) {
+  location.href = './views/level1.html';
+});
